337fa064b626ecaf40fa4bdef219d647ddb409a5
Abdullah Safi <safiabdullah769@gmail.com>
2026-05-31 14:23:18 +0430

    added some fanctionalities

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const guess = document.querySelector(".guess");
+const check_btn = document.querySelector(".check");
+const msg = document.querySelector(".message");
+const score = document.querySelector(".score");
+const high_score = document.querySelector(".highscore");
+const numberEl = document.querySelector(".number");
+
+
+
+
+check_btn.addEventListener('click', function() {
+
+    const random_num =Math.floor(Math.random()*20)+1;
+    
+    numberEl.textContent = random_num;
+})
